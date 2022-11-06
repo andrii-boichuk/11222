e22479303982ed128c0b05e888e2067246e7011b
andrii-boichuck <amuronovu4@gmail.com>
2022-11-06 17:59:00 +0200

    add anamnez and patientlist

--- js/script.js
+++ js/script.js
@@ -1,28 +1,4 @@
 
-const bigTabList = document.querySelectorAll('.big-tabs_item')
-const tabList = document.querySelectorAll('.big-tabs_top-item');
-tabList.forEach(item => {
-  item.addEventListener('click', (e) => {
-    tabList.forEach(el => { 
-      el.classList.remove('big-tabs_top-item--active');
-    });
-    item.classList.add('big-tabs_top-item--active');
-    
-    if (item.classList.contains('big-tabs_top-item--general')) {
-      console.log('big-tabs_top-item--general---111')
-      document.getElementById('content-general').classList.add('big-tabs_item--active')
-      // document.querySelectorAll('.big-tabs_item--general').classList.add('big-tabs_item--active');
-      document.getElementById('content-anamnez').classList.remove('big-tabs_item--active');
-      console.log('big-tabs_top-item--general')
-    }
-    if (item.classList.contains('big-tabs_top-item--anamnez')) {
-      console.log('big-tabs_top-item--anamnez-2222');
-      document.getElementById('content-anamnez').classList.add('big-tabs_item--active');
-      document.getElementById('content-general').classList.remove('big-tabs_item--active');
-      console.log('big-tabs_top-item--anamnez----4444');
-    }
-  });
-});
 
 const langlist = document.querySelectorAll('.lang-item');
 langlist.forEach(item => {
